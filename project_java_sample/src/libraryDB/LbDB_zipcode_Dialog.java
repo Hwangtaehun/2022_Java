3c4500dc47fe6a7c101ddd81283cd48c994a3209
Hwangtaehun <r283@naver.com>
2023-05-17 18:35:03 +0900

    Update LbDB_zipcode_Dialog.java

--- project_java_sample/src/libraryDB/LbDB_zipcode_Dialog.java
+++ project_java_sample/src/libraryDB/LbDB_zipcode_Dialog.java
@@ -5,7 +5,103 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.sql.*;
 
-/*참조 사이트: https://qh5944.tistory.com/44*/
+class Addresstool{
+	private String sidobasic[] = {"서울특별시", "부산광역시", "대전광역시", "대구광역시", "인천광역시", "광주광역시", "울산광역시",
+							 "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도",
+							 "세종특별자치시", "강원도", "경기도", "서울시", "부산시", "대전시", "대구시", "인천시", "광주시", "울산시",
+							 "충북", "충남", "전북", "전남", "경북", "경남", "제주도", "세종시"};
+	private String sigungusp[] = {"고양시", "성남시", "수원시", "안산시", "안양시", "용인시", "창원시", "포항시", "전주시", "천안시", "청주시",
+								  "고양",   "성남",  "수원",  "안산",   "안양",  "용인",  "창원",   "포항",  "전주",   "천안",  "청주"};
+	private String main[];
+	private LbDB_DAO db;
+	private int i;
+	public String sido, sigungu, eupmyun, doro, buildno1, buildno2, dong_hj, ri, jibun1, jibun2;
+	
+	public Addresstool() {}
+	public Addresstool(String str, LbDB_DAO db) {
+		this.db = db;
+		String word = str.trim();
+		main = word.split(" ");
+		sort();
+	}
+	
+	private void sort() {
+		for(i = 0; i < sidobasic.length; i++) {
+			if(main[0].equals(sidobasic[i])) {
+				if(i > 16) {
+					int num = i % 17;
+					sido = sidobasic[num];
+				}
+				else {
+					sido = main[0];
+				}
+			}
+		}
+		i = 0;
+		if(sido.isEmpty()) {
+			sigungu(main[i]);
+		}
+		else {
+			i++;
+			sigungu(main[i]);
+		}
+		
+		
+	}
+	
+	private void sigungu(String str) {
+		ResultSet rs;
+		String word = null;
+		for(int j = 0; j < sigungusp.length; j++) {
+			if(str.equals(sigungusp[j])) {
+				if(j > 10) {
+					int num = j % 11;
+					word += sigungusp[num];
+				}
+				else {
+					word += str;
+				}
+				i++;
+				word += main[i];
+			}
+		}
+		String sql = "SELECT DISTINCT `sigungu` FROM `address` WHERE `sigungu` LIKE '" + word + "%'";
+		rs = db.getResultSet(sql);
+		
+		try {
+			while(rs.next()) {
+				sigungu = rs.getString("sigungu");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(!sigungu.isEmpty()) {
+			i++;
+		}
+	}
+	
+	private void eupmyun(String str) {
+		ResultSet rs;
+		String sql = "SELECT DISTINCT `eupmyun` FROM `address` WHERE `eupmyun` LIKE '" + str + "%'";
+		rs = db.getResultSet(sql);
+		
+		try {
+			while(rs.next()) {
+				eupmyun = rs.getString("sigungu");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(!eupmyun.isEmpty()) {
+			i++;
+		}
+	}
+}
+
+
+/*https://qh5944.tistory.com/44*/
 
 public class LbDB_zipcode_Dialog extends JDialog implements WindowListener{
 	private LbDB_DAO db;
@@ -22,6 +118,10 @@ public class LbDB_zipcode_Dialog extends JDialog implements WindowListener{
 	
 	void initform() {
 		
+	}
+	
+	public void length(String str) {
+		if(str.length() < 2);
 	}
 	
 	@Override
