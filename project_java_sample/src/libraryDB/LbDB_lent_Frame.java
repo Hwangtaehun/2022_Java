e4495f353c75a3c420e2f6f85d29514ea3b0691e
Hwangtaehun <r283@naver.com>
2023-06-11 20:49:32 +0900

    Update LbDB_lent_Frame.java

--- project_java_sample/src/libraryDB/LbDB_lent_Frame.java
+++ project_java_sample/src/libraryDB/LbDB_lent_Frame.java
@@ -5,6 +5,15 @@ import javax.swing.*;
 import javax.swing.event.*;
 import java.sql.*;
 
-public class LbDB_lent_Frame {
-
+public class LbDB_lent_Frame extends LbDB_main_Frame {
+	public LbDB_lent_Frame() {}
+	public LbDB_lent_Frame(LbDB_DAO db, Client cl, String title) {
+		this.db = db;
+		this.cl = cl;
+		menu_title = title;
+		menuform();
+		Initform();
+	}
+	
+	
 }
